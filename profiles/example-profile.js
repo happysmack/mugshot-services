ad81d4ed43d0fadfba10a861b22bda0f3e45fa3f
mugshot-services <contact@syniba.com>
2018-03-30 18:56:07 -0400

    Remove example mp account ;)

--- profiles/example-profile.js
+++ profiles/example-profile.js
@@ -1,8 +1,8 @@
 let argv = require('minimist')(process.argv.slice(2));
 
 const profileInformation = {
-	mpAccountId: argv.mpAccountId || 'ezbVCBSNN7UQUdFTkNZX05ZXzMzMDA0XzQxNA',
-	mpContentType: argv.mpContentType || 'DETENTION_CENTER', // DETENTION_CENTER | VINE
+	mpAccountId: argv.mpAccountId || '',
+	mpContentType: argv.mpContentType || 'DETENTION_CENTER', // DETENTION_CENTER, VINE
 	mpUsername: argv.mpUsername || 'MOBILEPATROL_ANDROID_5308',
 	mpPassword: argv.mpPassword || '-99-28438760-113-99[544cbc72c712ae2f',
 	fbAppId: argv.fbAppId || '',
